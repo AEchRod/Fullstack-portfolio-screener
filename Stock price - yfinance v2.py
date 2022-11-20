1ced5320ba1d3d316b8cb498c7d1475e1c7ed728
Andrés <85241651+AEchRod@users.noreply.github.com>
2022-11-20 22:22:34 +0000

    Update Stock price - yfinance v2.py

--- Stock price - yfinance v2.py
+++ Stock price - yfinance v2.py
@@ -2,21 +2,11 @@ import os
 from flask import Flask, render_template, jsonify, redirect, url_for #request
 from form import PortfolioForm, csrf, time_frame_choices, time_interval_choices
 import yfinance as yf
-#import pandas as pd
-
-#from pandas_datareader import data as pdr
-#yf.pdr_override()
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 csrf.init_app(app)
-
-"""portfolio_filler = str(input("Please input tickers to add to your stock portfolio: "))
-#this breaks the user input into a list so we can check each individual ticker.
-portfolio_list = portfolio_filler.split(" ")
-time_period = str(input("Please select time frame (1d, 1mo): "))
-time_interval = str(input("Please select time interval (1h, 1d, 1wk): "))"""
 
 #print("Your porfolio is made up of: ", portfolio_list)
 
@@ -32,27 +22,19 @@ def retrieve_portfolio():
     forms = PortfolioForm()
 
     port_tick = str(forms.portfolio_ticker.data)
+    port_tick_split = port_tick.split(" ")
     tim_fr = dict(time_frame_choices).get(forms.time_frame.data)
     tim_int = dict(time_interval_choices).get(forms.time_interval.data)
 
-    data = yf.download(tickers=str(port_tick), period=str(tim_fr), interval=str(tim_int))
+    #this is a dataframe
+    data = yf.download(tickers=port_tick_split, period=str(tim_fr), interval=str(tim_int))
 
-    #df = pd.DataFrame(data)
-    #data.to_html() -> to convert the dataframe to HTML, data.values.tolist() if it was a Pandas dataframe
-    return jsonify(data.to_dict())
+    #data.to_html() -> to convert the dataframe to HTML, or data.values.tolist()
+    #return jsonify(data.to_dict())
+    return data.to_json()
 
-#tickers = yf.Tickers(portfolio_filler)
-#tickers.tickers.portfolio_filler.history(period=time_period)
-# get stock info
-#msft.info
-
-# get historical market data
-"""hist = msft.history(period="max")
-hist_1d = msft.history(period="1d")"""
-
-#print(msft.info)
-#print(hist_1d)
 
 if __name__ == "main":
     app.run(port=5000, debug= True)
+
 
