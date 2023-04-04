c3ee401f5262f5a03e62d6d43c4f4d936f6459ac
Andrés <85241651+AEchRod@users.noreply.github.com>
2023-04-04 17:46:09 +0100

    Update form.py

--- form.py
+++ form.py
@@ -4,11 +4,12 @@ from wtforms import StringField, SelectField, validators, SubmitField
 csrf = CSRFProtect()
 
 time_frame_choices = [('1d', '1d'), ('1mo', '1mo'), ('1y', '1y'), ('2y', '2y')]
-time_interval_choices = [('1h', '1h'), ('1d', '1d'), ('1wk', '1wk'), ('1mo', '1mo')]
+time_interval_choices = [('1h', '1h'), ('1d', '1d'), ('5d', '5d'), ('1wk', '1wk'), ('1mo', '1mo'), ('3mo', '3mo')]
+
 
 class PortfolioForm(FlaskForm): #inheriting from FlaskForm
 
-    portfolio_ticker = StringField(u'Please input tickers to add to your stock portfolio: ', [validators.DataRequired()])
+    portfolio_ticker = StringField(u'Please input ticker to add to your stock portfolio (data available since 2018): ', default='AAPL', validators=[validators.DataRequired()])
     time_frame = SelectField(u'Select time frame: ', choices=time_frame_choices)
     time_interval = SelectField(u'Select time interval: ', choices=time_interval_choices)
     submit = SubmitField('See your portfolio!')
