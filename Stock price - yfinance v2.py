9dd312d024dd324b9d5c12ea4ea2e1062a65e874
Andrés <85241651+AEchRod@users.noreply.github.com>
2023-04-04 17:45:33 +0100

    Update Stock price - yfinance v2.py

--- Stock price - yfinance v2.py
+++ Stock price - yfinance v2.py
@@ -1,16 +1,20 @@
 import os
-from flask import Flask, render_template, jsonify, redirect, url_for #request
+from flask import Flask, render_template, jsonify, redirect, url_for, request
 from form import PortfolioForm, csrf, time_frame_choices, time_interval_choices
 import yfinance as yf
+import pandas as pd
+import pytz
+import datetime as dt
+from pandas_datareader import data as pdr
+
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 csrf.init_app(app)
 
-#print("Your porfolio is made up of: ", portfolio_list)
-
 @app.route('/', methods=['GET', 'POST'])
+
 def start_page():
     form = PortfolioForm()
     if form.validate_on_submit():
@@ -19,22 +23,25 @@ def start_page():
 
 @app.route('/portfolio/', methods=['GET', 'POST'])
 def retrieve_portfolio():
-    forms = PortfolioForm()
 
-    port_tick = str(forms.portfolio_ticker.data)
-    port_tick_split = port_tick.split(" ")
-    tim_fr = dict(time_frame_choices).get(forms.time_frame.data)
-    tim_int = dict(time_interval_choices).get(forms.time_interval.data)
+    form = PortfolioForm()
+
+    ticker = form.portfolio_ticker.data
+
+    yf.pdr_override()
+
+    tz = pytz.timezone("America/New_York")
+    end = tz.localize(dt.datetime.now())
+    start = tz.localize(dt.datetime(2018, 1, 1))
+ 
 
     #this is a dataframe
-    data = yf.download(tickers=port_tick_split, period=str(tim_fr), interval=str(tim_int))
+    data = yf.download(tickers=ticker, start=start, end=end)
 
-    #data.to_html() -> to convert the dataframe to HTML, or data.values.tolist()
-    #return jsonify(data.to_dict())
+
+    #this converts the dataframe to json
     return data.to_json()
-
+    
 
 if __name__ == "main":
     app.run(port=5000, debug= True)
-
-
